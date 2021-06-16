70c00c88421f6d00a5821195de8c4ab9b70aa036
xXLeoXxOne <42474033+xXLeoXxOne@users.noreply.github.com>
2021-06-16 20:41:11 +0200

    Add files via upload

---
+++ utils.h
@@ -0,0 +1,15 @@
+//
+// Created by msk4x on 16.06.2021.
+//
+
+#ifndef SNAKE_UTILS_H
+#define SNAKE_UTILS_H
+
+
+void cls();
+
+void setCursorPosition(int x, int y);
+
+int randNum(int min,int max);
+
+#endif //SNAKE_UTILS_H
